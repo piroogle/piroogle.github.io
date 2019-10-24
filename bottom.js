a83fd883ae052653d614f33c767a51c0512613e2
Andrew <bigspeedfpv@gmail.com>
2019-10-24 19:15:24 +0000

    move js

---
+++ bottom.js
@@ -0,0 +1,17 @@
+const getURLParameters = url =>
+  (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce(
+    (a, v) => (
+      (a[v.slice(0, v.indexOf('='))] = v.slice(v.indexOf('=') + 1)), a
+    ),
+    {}
+  )
+
+console.log(getURLParameters(location.search).text)
+
+window.onload = () => {
+  if (getURLParameters(location.search).text !== undefined) {
+    document.getElementById('bottom').innerHTML = decodeURI(
+      getURLParameters(location.search).text
+    )
+  }
+}
